4e8811c1b903e5c2268a96796aa5b935b32d68a6
watter46 <w.aoki.14@gmail.com>
2022-10-19 01:09:21 +0900

    Bootstrapを導入する#7

--- backend/index.php
+++ backend/index.php
@@ -4,11 +4,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <title>Document</title>
+
 </head>
 <body>
-  <h1>Hello World!</h1>
-  <p>pタグを追加</p>
+<div class="d-flex justify-content-center p-3 mb-2 bg-danger text-white">
+  <h1>日報登録システム</h1>
+</div>
+
 
 <?php
 // DB接続（ローカル用）
@@ -27,20 +32,33 @@ try {
 }
 ?>
 
-<table>
-  <tr><th>ID</th><th>社員番号</th><th>始業時刻</th><th>終業時刻</th><th>休憩時間</th><th>業務内容</th><th>登録日時</th><th>更新日時</th></tr>
-  <?php foreach($result as $row): ?>
-  <tr>
-    <td><?php echo $row['id']; ?></td>
-    <td><?php echo $row['employee_id']; ?></td>
-    <td><?php echo $row['start_time']; ?></td>
-    <td><?php echo $row['end_time']; ?></td>
-    <td><?php echo $row['break']; ?></td>
-    <td><?php echo $row['summary']; ?></td>
-    <td><?php echo $row['created_at']; ?></td>
-    <td><?php echo $row['updated_at']; ?></td>
-  </tr>
-  <?php endforeach ?>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">employee_id</th>
+      <th scope="col">start_time</th>
+      <th scope="col">end_time</th>
+      <th scope="col">break</th>
+      <th scope="col">summary</th>
+      <th scope="col">created_at</th>
+      <th scope="col">updated_at</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($result as $row): ?>
+      <tr>
+        <th scope="row"><?php echo $row['id']; ?></th>
+        <td><?php echo $row['employee_id']; ?></td>
+        <td><?php echo $row['start_time']; ?></td>
+        <td><?php echo $row['end_time']; ?></td>
+        <td><?php echo $row['break']; ?></td>
+        <td><?php echo $row['summary']; ?></td>
+        <td><?php echo $row['created_at']; ?></td>
+        <td><?php echo $row['updated_at']; ?></td>
+      </tr>
+    <?php endforeach ?>
+  </tbody>
 </table>
 
 </body>
