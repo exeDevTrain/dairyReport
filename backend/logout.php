5083415c228432f1c254659605feda2bde630993
watter46 <w.aoki.14@gmail.com>
2022-10-24 21:42:04 +0900

    ログアウト画面の作成#20

--- backend/logout.php
+++ backend/logout.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Document</title>
-
+    
+    <style>
+        html,
+        body,
+        main {
+            height: 100%;
+        }
+    </style>
+    
+    <title>Logout Page</title>
 </head>
-
 <body>
-    <a href="./employees.php">社員一覧画面</a>
-    <a href="./logic/daily_reports.php">日報一覧画面</a>
-    <a href="./dailyReport_edit.php">日報編集画面</a>
-    <a href="./login.php">ログイン画面</a>
-    <a href="./logut.php">ログアウト画面</a>
+<main class="container w-75 d-flex align-items-center justify-content-center">
+        <div class="w-75 h-50 p-4 border border-2 border-secondary d-flex justify-content-center align-items-center ">
+            <h1 class="">お疲れ様でした。</h1>
+        </div>
+    </main>
 </body>
-
 </html>
